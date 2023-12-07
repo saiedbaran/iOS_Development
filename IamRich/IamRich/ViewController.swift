e5455802950fd3b8e861666f6e233816f31b2695
Saeed <s.safikhani@outlook.com>
2023-12-07 23:15:17 +0100

    initial commit

---
+++ IamRich/IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Saeed on 07.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
